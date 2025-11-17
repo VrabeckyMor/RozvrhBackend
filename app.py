7890f02c3ec72ce2cc8df1d5ae64aa6eefe3f0d0
Tadeáš Kótka <tadekotk@gmail.com>
2025-11-17 14:28:01 +0100

    upravení hostu app.run

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def get_array(token):
         return jsonify({'error': 'Not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
